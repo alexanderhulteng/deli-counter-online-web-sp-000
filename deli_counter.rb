e656710e189e71e61014d4d1a1214f37eeaab4ca
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-01-29 18:43:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number(array, name)
+ if array = []
+  array << name
+else
+  array << name
+end
+end
+
+def line(array)
+peopleInLine = ""
+  if array == []
+    puts "The line is currently empty."
+  else
+    array.each { |n| peopleInLine = peopleInLine + n }
+
+    end
+puts peopleInLine
+  end
+
+def now_serving(array)
+if array == []
+  puts "There is nobody waiting to be served!"
+end
+
+end
+line(katz_deli)
+now_serving(katz_deli)
