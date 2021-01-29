9519c99cd7ade77c7f9a7b63ef14a8a2662bc4fa
Alexander Hulteng <alexanderhulteng@gmail.com>
2021-01-29 18:45:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,21 @@
 katz_deli = []
 
 def take_a_number(array, name)
- 
+
   array << name
 
 end
 
 def line(array)
 peopleInLine = ""
+
   if array == []
     puts "The line is currently empty."
   else
     array.each { |n| peopleInLine = peopleInLine + n }
 
     end
-puts peopleInLine
+
   end
 
 def now_serving(array)
